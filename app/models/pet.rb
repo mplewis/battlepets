82af042a5a45a10465dc325136d07569b973045f
Matthew Lewis <matt@mplewis.com>
2016-07-09 17:17:51 -0600

    pet: initialize with values

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,25 @@
 class Pet < ActiveRecord::Base
+
+  before_create :set_initial_values
+
   has_many :performances
+
+  def set_initial_values
+    if rand(10) == 0
+      self.name = Faker::StarWars.character
+    else
+      self.name = Faker::Name.first_name
+    end
+    self.strength = initial_attribute
+    self.agility = initial_attribute
+    self.wit = initial_attribute
+    self.perception = initial_attribute
+  end
+
+  private
+
+  def initial_attribute
+    rand(2..9)
+  end
+
 end
