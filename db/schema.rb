8a33908ee847e26edb043d984c8cc3d3a87b0ce9
Matthew Lewis <matt@mplewis.com>
2016-07-09 16:43:44 -0600

    db: add schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709220621) do
+ActiveRecord::Schema.define(version: 20160709222544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "matches", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "performances", force: :cascade do |t|
+    t.float    "score"
+    t.integer  "match_id_id"
+    t.integer  "pet_id_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
