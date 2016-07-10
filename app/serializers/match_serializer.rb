fbb1ed41f7ecb7302d13546a48f2aee82b472fe7
Matthew Lewis <matt@mplewis.com>
2016-07-09 23:28:11 -0600

    view: add progress, completion to map

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,4 +1,16 @@
 class MatchSerializer < ActiveModel::Serializer
-  attributes :id, :winner
+  attributes :id, :complete, :winner, :progress
   has_many :performances
+
+  def winner
+    {id: object.winner.pet.id,
+     name: object.winner.pet.name,
+     score: object.winner.score}
+  end
+
+  def progress
+    {complete: object.performances.where(complete: true).count,
+     incomplete: object.performances.where(complete: false).count}
+  end
+
 end
