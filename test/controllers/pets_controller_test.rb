fd6df9fb376cd9b0b231d54ab0d2c573b1b38aee
Matthew Lewis <matt@mplewis.com>
2016-07-09 19:42:18 -0600

    test: add test for training pet

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -2,16 +2,17 @@ require 'test_helper'
 
 class PetsControllerTest < ActionController::TestCase
   setup do
-    @pet = pets(:one)
+    @pet = Pet.new
+    @pet.save!
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:pets)
   end
 
-  test "should create pet" do
+  test 'should create pet' do
     assert_difference('Pet.count') do
       post :create, pet: {  }
     end
@@ -19,21 +20,28 @@ class PetsControllerTest < ActionController::TestCase
     assert_response 201
   end
 
-  test "should show pet" do
+  test 'should show pet' do
     get :show, id: @pet
     assert_response :success
   end
 
-  test "should update pet" do
-    put :update, id: @pet, pet: {  }
+  test 'should update pet' do
+    put :update, id: @pet, pet: {name: 'Truffles'}
     assert_response 204
   end
 
-  test "should destroy pet" do
+  test 'should destroy pet' do
     assert_difference('Pet.count', -1) do
       delete :destroy, id: @pet
     end
 
     assert_response 204
+  end
+
+  test 'should train pet' do
+    before = @pet.strength
+    post :train, id: @pet, training: {type: 'lifting'}
+    @pet.reload
+    assert @pet.strength > before
   end
 end
