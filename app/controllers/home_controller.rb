712f00ba29a97173886ec87facc229056c825d81
Matthew Lewis <matt@mplewis.com>
2016-07-09 21:29:48 -0600

    view: leaderboard on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    render json: {'hello': 'world'}
+    leaders = Pet.find_each.map do |p|
+      {id: p.id, name: p.name, wins: p.wins}
+    end
+    leaders = leaders.sort {|left, right| right[:wins] <=> left[:wins]}.take 10
+    render json: {leaders: leaders}
   end
 end
